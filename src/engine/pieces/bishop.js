7d4ddae515b1c9b29c2bc5659aba16fa3e38b15f
Marion Maybury <Marion.Maybury@softwire.com>
2021-08-24 18:51:10 +0100

    add bishop moves, incomplete

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -15,35 +15,77 @@ export default class Bishop extends Piece {
         let locationCol = location.col;
 
         let expectedMoves = []
+
+        let topRightR = locationRow
+        let topRightC = locationCol
+        let topLeftR = locationRow
+        let topLeftC = locationCol
+        let bottomRightR = locationRow
+        let bottomRightC = locationCol
+        let bottomLeftR = locationRow
+        let bottomLeftC = locationCol
 // diagonal movement (+1,+1)(-1, -1)(-1, +1)(+1, -1)
         for(let i=0; i< initialArray.length; i++){
 
              // Top right
             if(locationRow != 7 && locationCol != 7){
-                let topRightR = locationRow++
-                let topRightC = locationCol++
+                topRightR++
+                topRightC++
                 expectedMoves.push(Square.at(topRightR, topRightC))
+                // locationRow = topRightR
+                // locationCol = topRightC
             }
             // Top left
             if(locationRow != 7 && locationCol != 0){
-                let topLeftR = locationRow++
-                let topLeftC = locationCol--
+                topLeftR++
+                topLeftC--
                 expectedMoves.push(Square.at(topLeftR, topLeftC))
+                // locationRow = topLeftR
+                // locationCol = topLeftC
+                
             }
             
             if(locationRow != 0 && locationCol != 7){
-                let bottomRightR = locationRow--
-                let bottomRightC = locationCol++
+                bottomRightR--
+                bottomRightC++
                 expectedMoves.push(Square.at(bottomRightR, bottomRightC))
+                // locationRow = bottomRightR
+                // locationCol = bottomRightC
             }
             if(locationRow != 0 && locationCol != 0){
-                let bottomLeftR = locationRow--
-                let bottomLeftC = locationCol--
+                bottomLeftR--
+                bottomLeftC--
                 expectedMoves.push(Square.at(bottomLeftR, bottomLeftC))
+                // locationRow = bottomLeftR
+                // locationCol = bottomLeftC
             }
 
 
         }
+
+        console.log("expectedMoves: ", expectedMoves)
         return expectedMoves;
     }
 }
+
+
+// function topRightMove(locationRow, locationCol, initialArray){
+
+//     let expectedTopRightMoves = []
+//     let topRightR = locationRow
+//     let topRightC = locationCol
+//     for(let i=0; i< initialArray.length; i++){
+
+//         // Top right
+//        if(locationRow != 7 && locationCol != 7){
+//           topRight ++
+//           topLeft++
+//         //    let topRightR = locationRow++
+//         //    let topRightC = locationCol++
+//            expectedTopRightMoves.push(Square.at(topRightR, topRightC))
+//         //    locationRow = topRightR
+//         //    locationCol = topRightC
+//        }
+//     }
+//     return expectedTopRightMoves
+// }
--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -1,5 +1,38 @@
+import 'chai/register-should';
 import Bishop from '../../../src/engine/pieces/bishop';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Bishop', () => {
+    
+    let board;
+    beforeEach(() => board = new Board());    
+    
+    it('can only move diagonally', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(1, 3), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // bottom left
+            Square.at(0,2),
+            //bottom right
+            Square.at(0,4),
+            // top right
+            Square.at(2,4),
+            Square.at(3,5),
+            Square.at(4,6),
+            Square.at(5,7),
+            // top left
+            Square.at(2,2),
+            Square.at(3,1),
+            Square.at(4,0)
+        ]
+        
+        moves.should.have.length(9);
+        moves.should.deep.include.members(expectedMoves);
+    });
 
 });
