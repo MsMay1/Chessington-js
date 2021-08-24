bdb07b41ff800d3099dc599fafcf6ba80ec8b8fd
Marion Maybury <Marion.Maybury@softwire.com>
2021-08-24 17:29:02 +0100

    create test and gameplay for king

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +8,32 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+
+        let location = board.findPiece(this)
+        let locationRow = location.row;
+        let locationCol = location.col;
+        let expectedMoves = []
+
+        // Movement down
+        if(locationRow != 0){
+            expectedMoves.push(Square.at(locationRow-1, locationCol))
+        }
+
+        // Movement up
+        if(locationRow != 7){
+            expectedMoves.push(Square.at(locationRow+1, locationCol))
+        }
+
+         // Movement right
+         if(locationCol != 0){
+            expectedMoves.push(Square.at(locationRow, locationCol-1))
+        }
+
+         // Movement left
+         if(locationCol != 7){
+            expectedMoves.push(Square.at(locationRow, locationCol+1))
+        }
+        console.log("kingExpectedMoves: ", expectedMoves)
+        return expectedMoves;
     }
 }
--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,5 +1,30 @@
+import 'chai/register-should';
 import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('King', () => {
+
+    let board;
+    beforeEach(() => board = new Board());    
+    
+    it('can only move one square', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(0, 4), king);
+ //       king.moveTo(board, Square.at(1, 4));
+
+        const moves = king.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(1,4),
+            Square.at(0,3),
+            Square.at(0,5)
+        ]
+        
+        moves.should.have.length(3);
+        moves.should.deep.include.members(expectedMoves);
+    });
+
 
 });
