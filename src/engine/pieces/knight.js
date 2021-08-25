18991b24f743b1021de583cb0a511ca93efe2020
Marion Maybury <Marion.Maybury@softwire.com>
2021-08-25 14:16:12 +0100

    knight movement and test

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
+
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +9,84 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        
+        let locationRow = location.row;
+        let locationCol = location.col;
+
+        let expectedMoves =[];
+
+        function topMove(locationRow, locationCol){
+            if(locationRow <= 5){
+                let row = locationRow + 2
+                let colLeft = locationCol - 1
+                let colRight = locationCol + 1
+
+                if(locationCol != 0) {
+                    expectedMoves.push(Square.at(row,colLeft))
+                }
+                if(locationCol != 7){
+                    expectedMoves.push(Square.at(row,colRight))
+                }    
+            }
+
+        }
+
+        function bottomMove(locationRow, locationCol){
+            if(locationRow >= 2){
+                let row = locationRow - 2
+                let colLeft = locationCol - 1
+                let colRight = locationCol + 1
+
+                if(locationCol != 0) {
+                    expectedMoves.push(Square.at(row,colLeft))
+                }
+                if(locationCol != 7){
+                    expectedMoves.push(Square.at(row,colRight))
+                }    
+            }
+
+        }
+
+        function rightMove(locationRow, locationCol){
+            if(locationCol <= 5){
+                let col = locationCol + 2
+                let rowLeft = locationRow - 1
+                let rowRight = locationRow + 1
+
+                if(locationRow != 0) {
+                    expectedMoves.push(Square.at(rowLeft,col))
+                }
+                if(locationRow != 7){
+                    expectedMoves.push(Square.at(rowRight,col))
+                }    
+            }
+
+        }
+
+        function leftMove(locationRow, locationCol){
+            if(locationCol >= 2){
+                let col = locationCol - 2
+                let rowLeft = locationRow - 1
+                let rowRight = locationRow + 1
+
+                if(locationRow != 0) {
+                    expectedMoves.push(Square.at(rowLeft,col))
+                }
+                if(locationRow != 7){
+                    expectedMoves.push(Square.at(rowRight,col))
+                }    
+            }
+
+        }
+
+        topMove(locationRow, locationCol)
+        bottomMove(locationRow, locationCol)
+        rightMove(locationRow, locationCol)
+        leftMove(locationRow, locationCol)
+
+        console.log("Knight: ", expectedMoves)
+
+        return expectedMoves;
     }
 }
--- tests/engine/pieces/knight.spec.js
+++ tests/engine/pieces/knight.spec.js
@@ -1,5 +1,31 @@
+import 'chai/register-should';
 import Knight from '../../../src/engine/pieces/knight';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
+
 
 describe('Knight', () => {
+    let board;
+    beforeEach(() => board = new Board());    
+    
+    it('can only move in "L" shapes', () => {
+        const knight = new Knight(Player.WHITE);
+        board.setPiece(Square.at(1, 1), knight);
+
+        const moves = knight.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(2,3),
+            Square.at(0,3),
+            Square.at(3,2),
+            Square.at(3,0),
+   
+        ]
+        
+        moves.should.have.length(4);
+        moves.should.deep.include.members(expectedMoves);
+    });
 
 });
+
