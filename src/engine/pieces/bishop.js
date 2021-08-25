62bd548d3cae16442eac9e2c14c06a92db8a1b4b
Marion Maybury <Marion.Maybury@softwire.com>
2021-08-25 12:36:34 +0100

    bishop movement and test

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -8,84 +8,93 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        let initialArray = [0, 1, 2, 3, 4, 5, 6, 7]
-        
+
+        let location = board.findPiece(this);
         let locationRow = location.row;
         let locationCol = location.col;
+        let expectedMoves =[];
 
-        let expectedMoves = []
+        function findMoves(locationRow, locationCol){
 
-        let topRightR = locationRow
-        let topRightC = locationCol
-        let topLeftR = locationRow
-        let topLeftC = locationCol
-        let bottomRightR = locationRow
-        let bottomRightC = locationCol
-        let bottomLeftR = locationRow
-        let bottomLeftC = locationCol
-// diagonal movement (+1,+1)(-1, -1)(-1, +1)(+1, -1)
-        for(let i=0; i< initialArray.length; i++){
+            let initialArray = [0, 1, 2, 3, 4, 5, 6, 7];
 
-             // Top right
-            if(locationRow != 7 && locationCol != 7){
-                topRightR++
-                topRightC++
-                expectedMoves.push(Square.at(topRightR, topRightC))
-                // locationRow = topRightR
-                // locationCol = topRightC
-            }
-            // Top left
-            if(locationRow != 7 && locationCol != 0){
-                topLeftR++
-                topLeftC--
-                expectedMoves.push(Square.at(topLeftR, topLeftC))
-                // locationRow = topLeftR
-                // locationCol = topLeftC
-                
-            }
-            
-            if(locationRow != 0 && locationCol != 7){
-                bottomRightR--
-                bottomRightC++
-                expectedMoves.push(Square.at(bottomRightR, bottomRightC))
-                // locationRow = bottomRightR
-                // locationCol = bottomRightC
-            }
-            if(locationRow != 0 && locationCol != 0){
-                bottomLeftR--
-                bottomLeftC--
-                expectedMoves.push(Square.at(bottomLeftR, bottomLeftC))
-                // locationRow = bottomLeftR
-                // locationCol = bottomLeftC
+            let availableTopRowMoves =[];
+            for(let j=(locationRow+1); j<=7; j++){
+                availableTopRowMoves.push(j)
             }
 
+            let availableRightColMoves =[];
+            for(let j=(locationCol+1); j<=7; j++){
+                availableRightColMoves.push(j)
+            }
+
+            let availableBottomRowMoves =[];
+            for(let j=(locationRow-1); j>=0; j--){
+                availableBottomRowMoves.push(j)
+            }
+  
+            let availableLeftColMoves =[];
+            for(let j=(locationCol-1); j>=0; j--){
+                availableLeftColMoves.push(j)
+            }
+
+            let moves = {
+                "availableTopRowMoves": availableTopRowMoves,
+                "availableRightColMoves": availableRightColMoves,
+                "availableBottomRowMoves": availableBottomRowMoves,
+                "availableLeftColMoves": availableLeftColMoves
+        }
+            console.log(moves)
+            console.log("FINDMOVES: moves.availableTopRowMoves: ", moves.availableTopRowMoves)
+            return moves
+        }
+
+        function smallestArray(x, y){
+            let arrayLength; 
+            if(x.length < y.length){
+                arrayLength = x.length
+            } else {
+                arrayLength = y.length
+            }
+            return arrayLength
+        }
+
+        function topRight(moves){
+            let movesLength = smallestArray(moves.availableTopRowMoves, moves.availableRightColMoves)  
+            for(let i=0; i<movesLength; i++){
+                expectedMoves.push(Square.at(moves.availableTopRowMoves[i],moves.availableRightColMoves[i]))
+            }
+        }
+
+        function topLeft(moves){
+            let movesLength = smallestArray(moves.availableTopRowMoves, moves.availableLeftColMoves)
+            for(let i=0; i<movesLength; i++){
+                expectedMoves.push(Square.at(moves.availableTopRowMoves[i],moves.availableLeftColMoves[i]))
+            }
+        }
+
+        function bottomLeft(moves){
+            let movesLength = smallestArray(moves.availableBottomRowMoves, moves.availableLeftColMoves)
+            for(let i=0; i<movesLength; i++){
+                expectedMoves.push(Square.at(moves.availableBottomRowMoves[i],moves.availableLeftColMoves[i]))
+            }
+        }
+
+        function bottomRight(moves){
+            let movesLength = smallestArray(moves.availableBottomRowMoves, moves.availableRightColMoves)
+            for(let i=0; i<movesLength; i++){
+                expectedMoves.push(Square.at(moves.availableBottomRowMoves[i],moves.availableRightColMoves[i]))
+            }
 
         }
 
-        console.log("expectedMoves: ", expectedMoves)
-        return expectedMoves;
+        let moves = findMoves(locationRow, locationCol);
+        topRight(moves); 
+        topLeft(moves)
+        bottomLeft(moves)
+        bottomRight(moves)
+        console.log(expectedMoves)
+        return expectedMoves
     }
+
 }
-
-
-// function topRightMove(locationRow, locationCol, initialArray){
-
-//     let expectedTopRightMoves = []
-//     let topRightR = locationRow
-//     let topRightC = locationCol
-//     for(let i=0; i< initialArray.length; i++){
-
-//         // Top right
-//        if(locationRow != 7 && locationCol != 7){
-//           topRight ++
-//           topLeft++
-//         //    let topRightR = locationRow++
-//         //    let topRightC = locationCol++
-//            expectedTopRightMoves.push(Square.at(topRightR, topRightC))
-//         //    locationRow = topRightR
-//         //    locationCol = topRightC
-//        }
-//     }
-//     return expectedTopRightMoves
-// }
--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -2,35 +2,6 @@ import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
 
-// export default class Pawn extends Piece {
-//     constructor(player) {
-//         super(player);
-//     }
-
-//     getAvailableMoves(board) {
-//         let location = board.findPiece(this)
-//         if (this.player === Player.WHITE) {
-//             if(location == (Square.at(1, location.col))){
-//                 console.log(Square.at(1, location.col))
-//                 console.log("Array2: ", [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)])
-//                 return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
-//             } else if (location == (Square.at(7,location.col))){
-//                 console.log("else if: ", Square.at(7,location.col))
-//                 throw "No Available moves"
-//             }
-//              console.log("Array1: ", [Square.at(location.row + 1, location.col)])
-//              return [Square.at(location.row + 1, location.col)]
-//         } else {
-//             if(location = Square.at(6, location.col)){
-//                 return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
-//             } else if (location == Square.at(0,location.col)){
-//                 throw "No Available moves"
-//             }
-//              return [Square.at(location.row - 1, location.col)]
-//         }
-//     }
-// }
-
 // TEST FUNCTION
 export default class Pawn extends Piece {
     constructor(player) {
